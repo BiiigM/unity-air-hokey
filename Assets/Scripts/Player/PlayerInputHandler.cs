f6d25307ddf251c09ce569860a6f9434d173188a
Maxim Bigler <maxim.bigler@frostfight.com>
2024-06-11 23:51:38 +0200

    Add function for two player with two mouses

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -1,11 +1,14 @@
 using System.Linq;
+using MultiMouseUnity;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    private float _cameraZDistance;
     private PlayerInput _playerInput;
     private PlayerMover _playerMover;
+    private Camera mainCamera;
 
     private void Awake()
     {
@@ -13,10 +16,38 @@ public class PlayerInputHandler : MonoBehaviour
         var playerMovers = FindObjectsByType<PlayerMover>(FindObjectsSortMode.None);
         var index = _playerInput.playerIndex;
         _playerMover = playerMovers.FirstOrDefault(mover => mover.GetPlayerIndex() == index);
+        MultiMouseWrapper.OnLeftMouseButtonDown[index] += HideCursor;
+        mainCamera = Camera.main;
+        _cameraZDistance = mainCamera.WorldToScreenPoint(_playerMover.transform.position).z;
+    }
+
+    private void Update()
+    {
+        if (!MultiMouseWrapper.Instance.IsMouseActive(_playerInput.playerIndex)) return;
+        var playerIndex = _playerInput.playerIndex;
+        var playerPosition = _playerMover.transform.position;
+        var mousePosition = MultiMouseWrapper.Instance.GetMousePosition(playerIndex);
+        var mouseWorldPosition =
+            mainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y,
+                _cameraZDistance));
+        var direction = (mouseWorldPosition - playerPosition).normalized;
+        if (Vector3.Distance(mouseWorldPosition, playerPosition) < 0.3)
+        {
+            _playerMover.OnMove(Vector2.zero);
+            return;
+        }
+
+        _playerMover.OnMove(new Vector2(direction.x, direction.z));
+    }
+
+    private void HideCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        Debug.Log(context.ReadValue<Vector2>());
         _playerMover.OnMove(context.ReadValue<Vector2>());
     }
 }
