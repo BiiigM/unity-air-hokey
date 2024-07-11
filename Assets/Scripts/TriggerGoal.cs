7dcd3cd2d9a344380b71eed8f1422477180ae1c8
Maxim Bigler <maxim.bigler@frostfight.com>
2024-07-11 15:07:19 +0200

    Add scoreboard

--- Assets/Scripts/TriggerGoal.cs
+++ Assets/Scripts/TriggerGoal.cs
@@ -1,16 +1,27 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerGoal : MonoBehaviour
 {
+    [SerializeField] private bool playerOne;
+
+    [SerializeField] private Text text;
+
     private void OnTriggerEnter(Collider other)
     {
         if ("puck".Equals(other.tag))
         {
-            ResetScript resetScript = other.GetComponent<ResetScript>();
+            var resetScript = other.GetComponent<ResetScript>();
             if (resetScript != null) resetScript.ResetObject();
         }
+
+        var oldText = text.text;
+        var playersText = oldText.Split(" : ").Select(int.Parse).ToArray();
+
+        if (playerOne)
+            text.text = playersText[0] + " : " + (playersText[1] + 1);
+        else
+            text.text = playersText[0] + 1 + " : " + playersText[1];
     }
 }
